404555e81af6f187a6da4a572eb75631dd0e46a0
Laurel <lolkinetzky@gmail.com>
2021-05-01 12:46:03 -0700

    added Planet model, route create_planets

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,11 @@ migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
-    app.congfig['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development
+    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
     db.init_app(app)
     migrate.init_app(app, db)
-    
+    from app.models.planet import Planet
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,21 @@
 from flask import Blueprint
 
+solar_system_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@solar_system_bp.route("/planets", methods=["POST"])
+#can we give it multiple methods?^
+def create_planets():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"],
+                        moons=request_body["moons"])
+    
+    db.session.add(new_planet)
+    deb.session.commit()
+
+    return f"Planet {new_planet.name} successfully created.", 201
+
+
+
+
+
